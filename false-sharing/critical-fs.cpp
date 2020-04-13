7050620ca5c1f5d08715d5f900998dfd2760fe6c
Xiaoyang Gong <13921568+realXGong@users.noreply.github.com>
2020-04-13 14:14:39 -0500

    Add files via upload

--- false-sharing/critical-fs.cpp
+++ false-sharing/critical-fs.cpp
@@ -1,10 +1,19 @@
+/*
+ * critical-fs tests false sharing on critical path
+ *
+ * Author: Xiaoyang Gong
+ */
 #include <assert.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <iostream>
+#include <random>
+#include <ctime>
 
 int main(int argc, const char** argv) {
+    srand(time(0));
     if (argc != 3) {
       printf("Usage: ./critical-fs <nthreads> <size>\n");
       exit(1);
@@ -26,24 +35,29 @@ int main(int argc, const char** argv) {
    
     printf("Setting OMP threads to %d\n", nthreads);
 
-    double sum=0.0, sum_local[nthreads], x[size], y[size];
-    for(int i = 0; i < size; i++){
-        x[i] = i;
-        y[i] = i*i - i;
+    double  sum = 0.0; 
+    double* sum_local = new double[nthreads];
+
+    for(int i = 0; i < nthreads; i++){
+        sum_local[i] = ((double)rand()) / ((double)RAND_MAX);
     }
-    printf("x,y initialized\n");
+    printf("sum_local initialized\n");
 
     printf("Start false sharing test...\n");
     #pragma omp parallel num_threads(nthreads)
     {
     int me = omp_get_thread_num();
-    sum_local[me] = 0.0;
     
-    #pragma omp for
-    for (int i = 0; i < size; i++)
-        sum_local[me] += x[i] * y[i];
-    
-    #pragma omp atomic
-    sum += sum_local[me];
+    #pragma omp for schedule(static, 1)
+    for (int i = 0; i < size; i++){
+        sum_local[me] = sum_local[me] + me + i;
+        #pragma omp atomic
+        sum += sum_local[me];
     }
+    }
+    
+    printf("Finish false sharing test\n");
+    exit(1);
 }
+
+
